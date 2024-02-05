5706b504c31d0522ffc73fe9b7eed4ed32361b2a
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2024-02-05 11:26:49 +0000

    protocol: type get

--- app/manifest.ts
+++ app/manifest.ts
@@ -693,7 +693,7 @@ export default function manifest(): MetadataRoute.Manifest {
     protocol_handlers: [
       {
         protocol: "web+pwaprotocol",
-        url: "/tea?type=%s",
+        url: "/",
       },
     ],
 
